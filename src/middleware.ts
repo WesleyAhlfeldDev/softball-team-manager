2b1ad558b5a4a41edd9f9c897b4eb2bd3ba513be
wahlfeldPPAtour <w.ahlfeld@ppatour.com>
2026-04-08 09:17:11 -0500

    Fix middleware edge function size

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import { getToken } from "next-auth/jwt";
 export async function middleware(req: NextRequest) {
   const token = await getToken({
     req,
-    secret: process.env.AUTH_SECRET,
+    secret: process.env.AUTH_SECRET ?? "",
   });
 
   const isLoggedIn = !!token;
